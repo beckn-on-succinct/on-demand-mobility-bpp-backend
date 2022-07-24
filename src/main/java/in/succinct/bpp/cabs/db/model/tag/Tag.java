be2df814942af539de3db3429fd176c2dc4223d2
Venky <venkatramanm@gmail.com>
2022-07-24 18:09:40 +0530

    Add Index

--- src/main/java/in/succinct/bpp/cabs/db/model/tag/Tag.java
+++ src/main/java/in/succinct/bpp/cabs/db/model/tag/Tag.java
@@ -1,16 +1,21 @@
 package in.succinct.bpp.cabs.db.model.tag;
 
+import com.venky.swf.db.annotations.column.COLUMN_DEF;
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.column.defaulting.StandardDefault;
+import com.venky.swf.db.annotations.column.indexing.Index;
 import com.venky.swf.db.model.Model;
 
 
 
 public interface Tag extends Model {
     @UNIQUE_KEY
+    @Index
     public String getTaggedModelName();
     public void setTaggedModelName(String taggedModelName);
 
     @UNIQUE_KEY
+    @Index
     public String getName();
     public void setName(String name);
 
