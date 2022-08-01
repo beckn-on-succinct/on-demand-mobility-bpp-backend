05e2ecf6138fbeb62a8199493ee284edd54c1deb
Venky <venkatramanm@gmail.com>
2022-08-02 00:56:36 +0530

    Participant

--- src/main/java/in/succinct/bpp/cabs/db/model/supply/VehicleDocument.java
+++ src/main/java/in/succinct/bpp/cabs/db/model/supply/VehicleDocument.java
@@ -1,11 +1,13 @@
 package in.succinct.bpp.cabs.db.model.supply;
 
+import com.venky.swf.db.annotations.column.pm.PARTICIPANT;
 import com.venky.swf.db.annotations.column.validations.Enumeration;
 import com.venky.swf.db.model.Model;
 
 public interface VehicleDocument extends VerifiableDocument, Model {
     String[] DOCUMENTS_NEEDED = new String[]{"RC","FITNESS","INSURANCE"};
 
+    @PARTICIPANT
     public Long getVehicleId();
     public void setVehicleId(Long id);
     public Vehicle getVehicle();
