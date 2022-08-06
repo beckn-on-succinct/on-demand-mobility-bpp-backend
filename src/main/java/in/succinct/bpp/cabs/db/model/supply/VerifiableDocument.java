2affad230b8a7db906012066e06ec93644f6dad0
chandraprakash kistaiahgari <reddyp148@gmail.com>
2022-08-06 14:44:43 +0530

    verification approve/reject api

--- src/main/java/in/succinct/bpp/cabs/db/model/supply/VerifiableDocument.java
+++ src/main/java/in/succinct/bpp/cabs/db/model/supply/VerifiableDocument.java
@@ -8,6 +8,7 @@ import com.venky.swf.db.annotations.column.relationship.CONNECTED_VIA;
 import com.venky.swf.db.annotations.column.ui.HIDDEN;
 import com.venky.swf.db.annotations.column.ui.PROTECTION;
 import com.venky.swf.db.annotations.column.ui.PROTECTION.Kind;
+import com.venky.swf.db.annotations.column.validations.Enumeration;
 
 import java.io.InputStream;
 import java.sql.Date;
@@ -15,13 +16,29 @@ import java.util.List;
 
 public interface VerifiableDocument  {
 
+    public static final String APPROVED="Approved";
+    public static final String REJECTED="Rejected";
+    public static final String PENDING="Pending";
+
+
     @IS_VIRTUAL
     public boolean isExpired();
+
+    @IS_VIRTUAL
+    @PROTECTION(Kind.NON_EDITABLE)
+    @Enumeration(APPROVED+","+REJECTED+","+PENDING)
+    public String getVerificationStatus();
+
 
     @PROTECTION(Kind.NON_EDITABLE)
     @COLUMN_DEF(StandardDefault.BOOLEAN_FALSE)
     public boolean isVerified();
     public void setVerified(boolean verified);
+
+    @PROTECTION(Kind.NON_EDITABLE)
+    @COLUMN_DEF(StandardDefault.BOOLEAN_FALSE)
+    public boolean isRejected();
+    public void setRejected(boolean rejected);
 
 
     public Date getValidFrom();
